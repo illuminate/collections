7b28e3207d3bb000257cf2b03bed241546d93c81
mattfoster7 <38736065+mattfoster7@users.noreply.github.com>
2021-08-26 14:37:04 -0500

    Make defaultValue mixed

--- Collection.php
+++ Collection.php
@@ -363,15 +363,16 @@ class Collection implements ArrayAccess, Enumerable
      * Get an attribute from the first item from the collection passing the given truth test.
      *
      * @param  string  $attribute
-     * @param  string  $defaultValue
+     * @param  mixed  $defaultValue
      * @param  callable|null  $callback
      * @param  mixed  $default
      * @return mixed
      */
-    public function firstAttribute(string $attribute, string $defaultValue = null, callable $callback = null, $default = null)
+    public function firstAttribute(string $attribute, $defaultValue = null, callable $callback = null, $default = null)
     {
         $item = $this->first($callback, $default);
-        return ($item || is_null($defaultValue)) ? $item->$attribute : $defaultValue;
+  
+        return $item ? $item->$attribute : $defaultValue;
     }
 
     /**
